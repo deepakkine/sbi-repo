334c78a58cf305d24315a1e2ec1ab0feca59b7d8
Om Hande <handeom>
2023-02-28 07:05:26 +0530

    did few changes

--- asap.java
+++ asap.java
@@ -1,5 +1,5 @@
 i'm deepak kine 
-i have completed my diploma in mechanical engineering 
-i have completed my diploma from gramin polytechnic
+i have completed my degree in mechanical engineering 
+i have completed my degree from sggs
 my hobby are playing cricket
 my fev crickter is virat kohli
