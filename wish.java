7674e140a9293293e43971aa307763f6481d1d44
Om Hande <handeom>
2023-03-02 20:30:38 +0530

    modified

--- wish.java
+++ wish.java
@@ -2,17 +2,15 @@
 hii, hello 
 
 hello class {
-<<<<<<< Updated upstream
   
   //task 80 changes
    m3()
    int s=20
-=======
  
  //task 69 changes
   m1()
   int j=10
->>>>>>> Stashed changes
+
  {  
 }
 
