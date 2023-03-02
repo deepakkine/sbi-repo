168268e8d865ead0795408ed59fafd835941b743
Om Hande <handeom>
2023-03-02 20:27:58 +0530

    changes i veriable

--- wish.java
+++ wish.java
@@ -2,10 +2,17 @@
 hii, hello 
 
 hello class {
+<<<<<<< Updated upstream
   
   //task 80 changes
    m3()
    int s=20
+=======
+ 
+ //task 69 changes
+  m1()
+  int j=10
+>>>>>>> Stashed changes
  {  
 }
 
